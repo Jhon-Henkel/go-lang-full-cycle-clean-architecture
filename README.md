821092d2f9ec8ceb1745050dc0dfa17d8a4640f6
Jhonatan Henkel <jhowrf@gmail.com>
2023-12-29 17:18:46 -0300

    build: start no projeto
    deixando projeto pronto para iniciar o desafio.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# go-lang-full-cycle-clean-architecture
+# Go-Lang Full Cycle clean architecture module
 Repositório para implementação do módulo de clean architecture do curso Go-Expert da Full Cycle
+
+## Desafio
+Olá devs!
+
+Agora é a hora de botar a mão na massa. Pra este desafio, você precisará criar o useCase de listagem das orders.
+
+Esta listagem precisa ser feita com:
+- Endpoint REST (GET /order)
+- Service ListOrders com GRPC
+- Query ListOrders GraphQL
+
+Não esqueça de criar as migrações necessárias e o arquivo api.http com a request para criar e listar as orders.
+
+## Rodando o projeto
+Iniciar o Docker que está na raiz do projeto, e em seguida rodar o comando:
+```bash
+cd cmd/ordersystem/
+go run main.go wire_gen.go
+```
---
+++ internal/infra/web/order_handler.go
@@ -0,0 +1,49 @@
+package web
+
+import (
+	"encoding/json"
+	"github.com/Jhon-Henkel/go-lang-full-cycle-clean-architecture/internal/usecase"
+	"net/http"
+
+	"github.com/Jhon-Henkel/go-lang-full-cycle-clean-architecture/internal/entity"
+	"github.com/Jhon-Henkel/go-lang-full-cycle-clean-architecture/pkg/events"
+)
+
+type WebOrderHandler struct {
+	EventDispatcher   events.EventDispatcherInterface
+	OrderRepository   entity.OrderRepositoryInterface
+	OrderCreatedEvent events.EventInterface
+}
+
+func NewWebOrderHandler(
+	EventDispatcher events.EventDispatcherInterface,
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreatedEvent events.EventInterface,
+) *WebOrderHandler {
+	return &WebOrderHandler{
+		EventDispatcher:   EventDispatcher,
+		OrderRepository:   OrderRepository,
+		OrderCreatedEvent: OrderCreatedEvent,
+	}
+}
+
+func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var dto usecase.OrderInputDTO
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createOrder := usecase.NewCreateOrderUseCase(h.OrderRepository, h.OrderCreatedEvent, h.EventDispatcher)
+	output, err := createOrder.Execute(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
